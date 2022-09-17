09bea5a9b6341f0eb24f997c5065b478486de502
Dlawhdtn <111211783+Dlawhdtn@users.noreply.github.com>
2022-09-17 14:51:15 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+package kr.lim.test;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		// int, short, byte, long - 정수
+		// double, float - 실수
+		// char - 문자
+		// String - 문자열
+		// boolean - 논리 값
+		// ++, --
+		int a = 10;
+		//a = a + 1;
+		//a += 1;
+		//a++; -> a를 쓰고 1을 더해서 저장해라
+		//++a; -> 먼저 1을 더하고 저장하고 a를 써라
+		
+		System.out.println(a++);
+		System.out.println(a);
+	}
+}
